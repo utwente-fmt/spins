6c02598c3c00229aa63f3a5b6f8a12183dbd51e6
Michael Weber <michaelw@cs.utwente.nl>
2011-02-07 13:44:34 +0100

    Generate and compile *spinja.* files into current directory

--- spinjal.sh
+++ spinjal.sh
@@ -20,20 +20,20 @@ while [ $# -gt 1 ] ; do
     spinja_options="$spinja_options $1"
     shift
 done
-promela_file=$1
-output_file="$1.spinja.c"
+promela_file="$1"
+output_file="$(basename "$promela_file").spinja.c"
 
-if [ -f $output_file ]; then
-	rm $output_file;
+if [ -f "$output_file" ]; then
+	rm "$output_file";
 fi
 
-java  -cp "$script_dir/spinja.jar"   spinja.Compile -o3 -l $promela_file
-if [ ! -f $output_file ]; then
+java  -cp "$script_dir/spinja.jar"   spinja.Compile -o3 -l "$promela_file"
+if [ ! -f "$output_file" ]; then
 	echo "Compilation of $promela_file failed"
 	exit;
 fi
 
-gcc -fPIC -shared $output_file -o $promela_file.spinja -O0 -g $spinja_options
+gcc -fPIC -shared "$output_file" -o "$(basename "$output_file" .spinja.c).spinja" -O0 -g "$spinja_options"
 if [ ! $? -eq 0 ]; then
 	echo "Compilation of $output_file failed"
 fi
