17b3a45e5a9f32c4064ae0b0a972bb4147df87be
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2012-02-22 01:28:56 +0100

    Forgotten ChannelReadExpression

---
+++ src/spinja/promela/compiler/expression/ChannelReadExpression.java
@@ -0,0 +1,87 @@
+// Copyright 2010, University of Twente, Formal Methods and Tools group
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package spinja.promela.compiler.expression;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import spinja.promela.compiler.parser.ParseException;
+import spinja.promela.compiler.parser.Token;
+import spinja.promela.compiler.variable.Variable;
+import spinja.promela.compiler.variable.VariableAccess;
+import spinja.promela.compiler.variable.VariableType;
+import spinja.util.StringWriter;
+
+public class ChannelReadExpression extends Expression implements CompoundExpression {
+	private final Identifier id;
+
+	private final List<Expression> exprs;
+
+	public ChannelReadExpression(final Token token, final Identifier id) {
+		super(token);
+		this.id = id;
+		exprs = new ArrayList<Expression>();
+	}
+
+	public void addExpression(final Expression expr) {
+		exprs.add(expr);
+		if (!(expr instanceof Identifier)) {
+			for (final VariableAccess va : expr.readVariables()) {
+				va.getVar().setRead(true);
+			}
+		} else {
+			((Identifier) expr).getVariable().setWritten(true);
+		}
+	}
+
+	@Override
+	public String getBoolExpression() throws ParseException {
+		return "_channels[SPINJA].length()"; //TODO: SpinJa semantics
+	}
+
+	@Override
+	public String toString() {
+		final StringWriter w = new StringWriter();
+		w.append(id).append("?");
+		for (final Expression expr : exprs) {
+			w.append(expr.toString()).append(",");
+		}
+		w.setLength(w.length() - 1);
+		return w.toString();
+	}
+
+	public Identifier getIdentifier() {
+		return id;
+	}
+
+	public List<Expression> getExprs() {
+		return exprs;
+	}
+
+	@Override
+	public VariableType getResultType() throws ParseException {
+		return VariableType.BOOL;
+	}
+
+	@Override
+	public Set<VariableAccess> readVariables() {
+		final Set<VariableAccess> rv = new HashSet<VariableAccess>();
+		for (final Expression e : exprs)
+			rv.addAll(e.readVariables());
+		return rv;
+	}
+}
